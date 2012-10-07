4512706653dcf2ae081397c7c68423e3d134e432
unknown <venky@venky-PC.(none)>
2012-10-07 11:49:07 +0530

    Test

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KaratsubaMultiplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CallMul();
+            
+        }
+
+
+        public void CallMul()
+        {
+
+            Multiplication mul = new Multiplication();
+           double vaa=  mul.Karatsuba(123, 456, 4);
+
+          //Till not the karastsuba multiplication is working fine but finer analysis is required for core understanding 
+
+          //
+        }
+      
+   
+
+
+    }
+
+    
+}
---
+++ Multiplication.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KaratsubaMultiplication
+{
+    public class Multiplication
+    {
+        public double val = 0;
+        public double Karatsuba(double number1, double number2, int no_of_digits)
+        {
+            //double digi1 = number1 / 10;
+            //double digi2 = number2 / 10;
+
+            Console.WriteLine(String.Format("The number is {0} and {1}", number1, number2));
+            if ( no_of_digits == 1) 
+            {
+                return (number1 * number2);
+            }
+
+            //int myfactor = no_of_digits / 2;
+            double powerfactor = no_of_digits / 2;
+            powerfactor = Math.Ceiling(powerfactor);
+            double factor = Math.Pow(10, powerfactor);
+            double a = number1 / factor;
+            double c = number2 / factor;
+
+            double b = number1 % factor;
+            double d = number2 % factor;
+
+
+            a = Math.Truncate(a);
+            b = Math.Truncate(b);
+            c = Math.Truncate(c);
+            d = Math.Truncate(d);
+
+
+            double total = (Math.Pow(10, no_of_digits) * Karatsuba(a, c, no_of_digits / 2))
+                + (Math.Pow(10, no_of_digits / 2) * (Karatsuba(a, d, no_of_digits / 2) + Karatsuba(b, c, no_of_digits / 2)))
+
+              + (Karatsuba(b, d, no_of_digits / 2));
+
+
+       
+
+            Console.WriteLine(String.Format("Sum for numbers {0} and {1} is {2} ", number1, number2, total));
+            return total;
+        }
+
+    }
+}
